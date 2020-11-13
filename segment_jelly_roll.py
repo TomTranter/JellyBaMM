328fc5bd128557a62594a557e409e7aa9bf2368b
tomtranter <t.g.tranter@gmail.com>
2020-11-13 16:42:08 +0000

    continue tidy

--- segment_jelly_roll.py
+++ segment_jelly_roll.py
@@ -7,6 +7,7 @@ Created on Wed Oct 28 10:31:07 2020
 import ecm 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 plt.close('all')
 # Return the average of the input images, the mid-half-span and a distance
@@ -25,8 +26,8 @@ im_soft, cc_im = ecm.label_layers(im_soft, dt, mhs,
                              can_width=30,
                              im_thresh=19000,
                              small_feature_size=20000)
-np.savez('im_soft', im_soft)
-np.savez('cc_im', cc_im)
+np.savez(os.path.join(ecm.INPUT_DIR, 'im_soft'), im_soft)
+np.savez(os.path.join(ecm.INPUT_DIR, 'cc_im'), cc_im)
 # Make the spider web network
 net = ecm.spider_web_network(im_soft, mhs, cc_im, dtheta=10)
 
--- segment_jelly_roll_from_im.py
+++ segment_jelly_roll_from_im.py
@@ -7,10 +7,12 @@ Created on Wed Oct 28 13:33:07 2020
 import numpy as np
 import ecm
 import matplotlib.pyplot as plt
+import os
+
 plt.close('all')
 
-im_soft = np.load('im_soft.npz')['arr_0']
-cc_im = np.load('cc_im.npz')['arr_0']
+im_soft = np.load(os.path.join(ecm.INPUT_DIR, 'im_soft.npz'))['arr_0']
+cc_im = np.load(os.path.join(ecm.INPUT_DIR, 'cc_im.npz'))['arr_0']
 # Make the spider web network
 mhs = int(cc_im.shape[0]/2)
 net = ecm.spider_web_network(im_soft, mhs, cc_im, dtheta=20)
