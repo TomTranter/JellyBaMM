1d1985c092d1493d04fed9c50d830a067eca6cb2
tomtranter <t.g.tranter@gmail.com>
2020-11-17 15:19:13 +0000

    style __segment_jellyroll__

--- ecm/__segment_jellyroll__.py
+++ ecm/__segment_jellyroll__.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage.measure import regionprops as rp
 import scipy.ndimage as spim
-from skimage.morphology import binary_erosion, binary_dilation, medial_axis, disk, square
+from skimage.morphology import binary_erosion, binary_dilation
+from skimage.morphology import medial_axis, disk, square
 from skimage.measure import label
 import skimage.filters.rank as rank
 from skimage.segmentation import flood_fill
@@ -21,10 +22,10 @@ import ecm
 
 wrk = op.Workspace()
 
+
 def average_images(path=None):
     if path is None:
         path = ecm.INPUT_DIR
-    
     # Get File list
     files = []
     for file in os.listdir(path):
@@ -35,7 +36,7 @@ def average_images(path=None):
     masks = []
     boxes = []
     mids = []
-    # Find mid points of can 
+    # Find mid points of can
     for i in range(len(files)):
         can = ims[i] < 25000
         sel_ero = np.ones([5, 5])
@@ -52,7 +53,7 @@ def average_images(path=None):
     boxes = np.asarray(boxes)
     x_span = boxes[:, 2] - boxes[:, 0]
     y_span = boxes[:, 3] - boxes[:, 1]
-    mhs = np.int(np.ceil(np.min([x_span.min(), y_span.min()])/2)) # min half span
+    mhs = np.int(np.ceil(np.min([x_span.min(), y_span.min()])/2))  # min half span
     # Factorize image size - used for adaptive histogran
     factors = []
     for i in np.arange(2, mhs*2):
@@ -61,7 +62,11 @@ def average_images(path=None):
     # Crop images
     crops = []
     for i, (x_min, y_min, x_max, y_max) in enumerate(boxes):
-        crops.append(ims[i][mids[i, 0]-mhs:mids[i, 0]+mhs, mids[i, 1]-mhs:mids[i, 1]+mhs]*masks[i][mids[i, 0]-mhs:mids[i, 0]+mhs, mids[i, 1]-mhs:mids[i, 1]+mhs])
+        xl = mids[i, 0] - mhs
+        xh = mids[i, 0] + mhs
+        yl = mids[i, 1] - mhs
+        yh = mids[i, 1] + mhs
+        crops.append(ims[i][xl:xh, yl:yh]*masks[i][xl:xh, yl:yh])
         print(i, crops[-1].shape)
     # Pick image - replace with average
     im = crops[0].copy()
@@ -77,12 +82,13 @@ def get_radial_average(im, step, dt):
     band_avs = []
     r = []
     for i in range(len(bands)-1):
-        l = bands[i]
-        u = bands[i+1]
-        mask = (dt >= l) * (dt < u)
+        lower = bands[i]
+        upper = bands[i+1]
+        mask = (dt >= lower) * (dt < upper)
         band_avs.append(np.mean(im[mask]))
-        r.append((l+u)/2)
+        r.append((lower+upper)/2)
     return np.vstack((r, band_avs)).T
+
 
 def adjust_radial_average(im, step, deg, dt):
     mid = np.floor(np.array(im.shape)/2).astype(int)
@@ -90,11 +96,11 @@ def adjust_radial_average(im, step, deg, dt):
     band_avs = []
     r = []
     for i in range(len(bands)-1):
-        l = bands[i]
-        u = bands[i+1]
-        mask = (dt >= l) * (dt < u)
+        lower = bands[i]
+        upper = bands[i+1]
+        mask = (dt >= lower) * (dt < upper)
         band_avs.append(np.mean(im[mask]))
-        r.append((l+u)/2)
+        r.append((lower+upper)/2)
     dat = np.vstack((r, band_avs)).T
     p = np.polyfit(dat[:, 0], dat[:, 1], deg=deg)
     pfunc = np.poly1d(p)
@@ -102,24 +108,24 @@ def adjust_radial_average(im, step, deg, dt):
     adj = adj - adj[0]
     adj_im = im.copy().astype(float)
     for i in range(len(bands)-1):
-        l = bands[i]
-        u = bands[i+1]
-        mask = (dt >= l) * (dt < u)
+        lower = bands[i]
+        upper = bands[i+1]
+        mask = (dt >= lower) * (dt < upper)
         adj_im[mask] -= adj[i]
     return adj_im
 
 
 def remove_beam_hardening(im, dt, step, deg):
     # Compute radial averages and adjustments for beam hardening - soften image
-
     dat_im = ecm.get_radial_average(im, step, dt)
     im_soft = ecm.adjust_radial_average(im, step, deg, dt)
     dat_soft = ecm.get_radial_average(im_soft, step, dt)
-    fig, axes  = plt.subplots(2, 1)
+    fig, axes = plt.subplots(2, 1)
     axes[0].imshow(im_soft - im.astype(float))
     axes[1].plot(dat_im[:, 0], dat_im[:, 1])
     axes[1].plot(dat_soft[:, 0], dat_soft[:, 1])
     return im_soft
+
 
 def label_layers(im, dt, mhs, can_width=30, im_thresh=19000, small_feature_size=20000):
     # Remove outer
@@ -131,13 +137,10 @@ def label_layers(im, dt, mhs, can_width=30, im_thresh=19000, small_feature_size=
     # Final layer labels
     layers = np.zeros(can.shape, dtype=int)
     layers[can] = layers.max() + 1
-    
-    # Threshold image 
+    # Threshold image
     mask = im > im_thresh
     plt.figure()
     plt.imshow(mask)
-    
-    
     # Label threshold - Should result in 2 or 3 regions - if not look at threshold
     lab = label(mask)
     plt.imshow(lab)
@@ -145,15 +148,13 @@ def label_layers(im, dt, mhs, can_width=30, im_thresh=19000, small_feature_size=
     itmfrq = np.unique(lab, return_counts=True)
     itmfrq = np.vstack(itmfrq).T
     itmfrq = itmfrq[1:]
-    
     # Get rid of small and medium size edge features
     itmfrq = itmfrq[itmfrq[:, 1] > small_feature_size]
-    
     cc_im = np.zeros(lab.shape, dtype=np.int8)
-    
-    for l, _ in itmfrq:
-        if l > 0:
-            tmp = lab == l
+
+    for l_int, _ in itmfrq:
+        if l_int > 0:
+            tmp = lab == l_int
             layers[tmp] = layers.max() + 1
             tmp = binary_erosion(binary_dilation(tmp, disk(6)), disk(4))
             skel = medial_axis(tmp, return_distance=False)
@@ -171,11 +172,11 @@ def label_layers(im, dt, mhs, can_width=30, im_thresh=19000, small_feature_size=
                 tmp2 = flood_fill(tmp2, (seeds_x[i], seeds_y[i]), 2)
             tmp2[tmp2 == 2] = 0
             tmp2[skel_rank_sum > 3] = 1
-            cc_im[tmp2 > 0] = l
+            cc_im[tmp2 > 0] = l_int
             layers[tmp2 > 0] = layers.max() + 1
             plt.figure()
             plt.imshow(tmp2)
-    
+
     # get inner section
     cc_all = cc_im > 0
     cc_all = binary_dilation(cc_all, disk(8))
@@ -188,9 +189,10 @@ def label_layers(im, dt, mhs, can_width=30, im_thresh=19000, small_feature_size=
     mask_nan = lab == inner_lab
     im[mask_nan] = 0
     plt.figure()
-    im[im==0] = np.nan
+    im[im == 0] = np.nan
     plt.imshow(im)
     return im, cc_im
+
 
 def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
                        path=None, filename='spider_net'):
@@ -210,19 +212,18 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     # Extract coordinates of nodes on the current collectors
     cc_coords = []
     for i in np.unique(cc_im)[np.unique(cc_im) < 0]:
-        lab, N = label(cc_im == i, return_num=True)    
+        lab, N = label(cc_im == i, return_num=True)
         coords = np.zeros([N, 2], dtype=int)
-        for l in np.arange(N):
-            x, y = np.where(lab==l+1)
+        for l_int in np.arange(N):
+            x, y = np.where(lab == l_int+1)
             if len(x) > 0:
-                coords[l] = [np.int(np.around(np.mean(x),0)),
-                             np.int(np.around(np.mean(y),0))]
+                coords[l_int] = [np.int(np.around(np.mean(x), 0)),
+                                 np.int(np.around(np.mean(y), 0))]
         cc_coords.append(coords)
     plt.figure()
     plt.imshow(cc_im)
     plt.scatter(cc_coords[0][:, 1], cc_coords[0][:, 0], c='r', s=50)
     plt.scatter(cc_coords[1][:, 1], cc_coords[1][:, 0], c='b', s=50)
-    
     # Collect coords together
     coords = np.vstack((cc_coords[0], cc_coords[1]))
     # Record which belongs to same cc
@@ -233,15 +234,13 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     bins = np.arange(-180, 180+2*dtheta, dtheta)-dtheta/2
     point_group_theta = np.zeros(len(coords[:, 0]), dtype=int)
     for i in range(len(bins)-1):
-        l = bins[i]
-        u = bins[i+1]
-        sel = (point_thetas > l) * (point_thetas < u)
+        lower = bins[i]
+        upper = bins[i+1]
+        sel = (point_thetas > lower) * (point_thetas < upper)
         point_group_theta[sel] = i
     # Get radial position of coords
     rads = np.linalg.norm(coords - mhs, axis=1)
 
-
-    ###############################################################################
     groups, counts = np.unique(point_group_theta, return_counts=True)
     sorted_groups = np.zeros([len(groups), counts.max()], dtype=int)
     sorted_groups.fill(-1)
@@ -258,19 +257,18 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     inner_Ps = sorted_groups[:, 0]
     inner_cc = first_cc[inner_Ps]
     change_cc = inner_cc != np.roll(inner_cc, -1)
-    sorted_cc =  first_cc[sorted_groups].astype(int)
+    sorted_cc = first_cc[sorted_groups].astype(int)
 
     arc_indices = []
     cc_roll = [[], []]
     for cc_num, this_cc in enumerate([True, False]):
-        start_group = np.argwhere(np.logical_and(inner_cc==this_cc, change_cc)).flatten()[0]
+        start_group = np.argwhere(np.logical_and(inner_cc == this_cc,
+                                                 change_cc)).flatten()[0]
         layer_group = np.arange(0, len(groups), 1)
         layer_group = np.roll(layer_group, -start_group)
         i = 0
         layer = 0
-    
-        #this_cc = True
-        numbered_cc =  first_cc[sorted_groups].astype(int)
+        numbered_cc = first_cc[sorted_groups].astype(int)
         while layer < counts.max():
             g = layer_group[0]
             if sorted_cc[g, layer] == this_cc:
@@ -286,7 +284,7 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
                 else:
                     pass
             layer_group = np.roll(layer_group, 1)
-    
+
         plt.figure()
         plt.imshow(numbered_cc)
     print(len(np.unique(cc_roll[0]+cc_roll[1])))
@@ -317,12 +315,13 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
                 pos_inner_conns.append([i+len(ordered_neg_Ps), sorted_neighbor])
     neg_inner_conns = np.asarray(neg_inner_conns)
     pos_inner_conns = np.asarray(pos_inner_conns)
-    
+
     new_coords = np.vstack((neg_coords, pos_coords))
-    coords_3d = np.vstack((new_coords[:, 0], new_coords[:, 1], np.zeros(new_coords.shape[0]))).T
+    coords_3d = np.vstack((new_coords[:, 0], new_coords[:, 1],
+                           np.zeros(new_coords.shape[0]))).T
     new_conns = np.vstack((neg_conns, pos_conns, neg_inner_conns, pos_inner_conns))
     net = op.network.GenericNetwork(conns=new_conns, coords=coords_3d)
-    
+
     Ps, counts = np.unique(np.hstack((net['throat.conns'][:, 0],
                                       net['throat.conns'][:, 1])), return_counts=True)
     net['pore.surface'] = False
@@ -338,13 +337,14 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     net['pore.cell_id'] = np.arange(0, net.Np, 1).astype(int)
     net['pore.arc_index'] = np.asarray(arc_indices)
     net['pore.radial_position'] = np.linalg.norm(new_coords - mhs, axis=1)
-    inner_mask = np.logical_and(net['pore.surface'], net['pore.radial_position'] < mhs/2)
-    outer_mask = np.logical_and(net['pore.surface'], net['pore.radial_position'] > mhs/2)
+    rad_pos = net['pore.radial_position']
+    inner_mask = np.logical_and(net['pore.surface'], rad_pos < mhs/2)
+    outer_mask = np.logical_and(net['pore.surface'], rad_pos > mhs/2)
     net['pore.inner'][inner_mask] = True
     net['pore.outer'][outer_mask] = True
     net['throat.neg_cc'] = False
     net['throat.pos_cc'] = False
-    
+
     net['throat.spm_resistor'] = True
     pos_cc_Ts = net.find_neighbor_throats(net.pores("pos_cc"), mode="xnor")
     neg_cc_Ts = net.find_neighbor_throats(net.pores("neg_cc"), mode="xnor")
@@ -353,11 +353,14 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     net['throat.spm_resistor'][neg_cc_Ts] = False
     net['throat.spm_resistor'][pos_cc_Ts] = False
     net['throat.spm_resistor_order'] = -1
-    net['throat.spm_resistor_order'][net['throat.spm_resistor']] = np.arange(0, net.num_throats('spm_resistor'), 1).astype(int)
+    spm_res = net['throat.spm_resistor']
+    n_spm = net.sum(spm_res)
+    net['throat.spm_resistor_order'][spm_res] = np.arange(0, n_spm, 1, dtype=int)
     net['throat.spm_neg_inner'] = False
     net['throat.spm_pos_inner'] = False
-    net['throat.spm_neg_inner'][(net['throat.spm_resistor_order'] > -1) * (net['throat.spm_resistor_order'] < len(neg_inner_conns))] = True
-    net['throat.spm_pos_inner'][(net['throat.spm_resistor_order'] > -1) * (net['throat.spm_resistor_order'] >= len(neg_inner_conns))] = True
+    res_order = net['throat.spm_resistor_order']
+    net['throat.spm_neg_inner'][spm_res * (res_order < len(neg_inner_conns))] = True
+    net['throat.spm_pos_inner'][spm_res * (res_order >= len(neg_inner_conns))] = True
     net['pore.neg_tab'] = False
     net['pore.pos_tab'] = False
     net['pore.neg_tab'][net.pores(['inner', 'neg_cc', 'terminal'], mode='and')] = True
@@ -376,30 +379,31 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
     free_conns = np.vstack((np.arange(0, num_free, 1),
                             np.arange(0, num_free, 1)+num_free)).T
     free_conns += start
-    tt.extend(net, pore_coords=free_coords, throat_conns=free_conns, labels=['free_stream'])
-    net['pore.arc_index'][net['pore.free_stream']] = net['pore.arc_index'][net['pore.outer']]
+    tt.extend(net, pore_coords=free_coords,
+              throat_conns=free_conns, labels=['free_stream'])
+    free_Ps = net['pore.free_stream']
+    net['pore.arc_index'][free_Ps] = net['pore.arc_index'][net['pore.outer']]
     net['pore.cell_id'][net.pores('free_stream')] = -1
     net['pore.cell_id'] = net['pore.cell_id'].astype(int)
 
-    fig, ax = plt.subplots(figsize=(12, 12))  
-    fig=ecm.plot_resistors(net, net.throats('neg_cc'), c='r', fig=fig)
-    fig=ecm.plot_resistors(net, net.throats('pos_cc'), c='b', fig=fig)
-    fig=ecm.plot_resistors(net, net.throats('spm_resistor'), c='k', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('neg_cc'), c='r', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('pos_cc'), c='b', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('surface'), c='k', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('outer'), c='pink', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('inner'), c='purple', fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('terminal'), c='y', s=100, fig=fig)
-    fig=tt.plot_coordinates(net, pores=net.pores('free_stream'), c='g', fig=fig)
-    fig=tt.plot_connections(net, throats=net.throats('free_stream'), c='g', fig=fig)
+    fig, ax = plt.subplots(figsize=(12, 12))
+    fig = ecm.plot_resistors(net, net.throats('neg_cc'), c='r', fig=fig)
+    fig = ecm.plot_resistors(net, net.throats('pos_cc'), c='b', fig=fig)
+    fig = ecm.plot_resistors(net, net.throats('spm_resistor'), c='k', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('neg_cc'), c='r', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('pos_cc'), c='b', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('surface'), c='k', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('outer'), c='pink', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('inner'), c='purple', fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('terminal'), c='y', s=100, fig=fig)
+    fig = tt.plot_coordinates(net, pores=net.pores('free_stream'), c='g', fig=fig)
+    fig = tt.plot_connections(net, throats=net.throats('free_stream'), c='g', fig=fig)
 
     fig, ax = plt.subplots(figsize=(12, 12))
-    fig=ecm.plot_resistors(net, net.throats('neg_cc'), c='r', fig=fig)
-    fig=ecm.plot_resistors(net, net.throats('pos_cc'), c='b', fig=fig)
-    fig=ecm.plot_resistors(net, net.throats('spm_resistor'), c='k', fig=fig)
+    fig = ecm.plot_resistors(net, net.throats('neg_cc'), c='r', fig=fig)
+    fig = ecm.plot_resistors(net, net.throats('pos_cc'), c='b', fig=fig)
+    fig = ecm.plot_resistors(net, net.throats('spm_resistor'), c='k', fig=fig)
     plt.imshow(im_soft.T)
-
 
     # Scale and save net
     prj = wrk['proj_01']
@@ -415,4 +419,3 @@ def spider_web_network(im_soft, mhs, cc_im, dtheta=10, pixel_size=10.4e-6,
         path = ecm.INPUT_DIR
     wrk.save_project(project=prj, filename=os.path.join(path, filename))
     return net
-    
