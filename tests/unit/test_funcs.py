e9f8c524b31a1efa83ccb93fadbc5c3b7968c325
tomtranter <t.g.tranter@gmail.com>
2020-11-18 14:25:53 +0000

    make tests work

--- tests/unit/test_funcs.py
+++ tests/unit/test_funcs.py
@@ -6,12 +6,44 @@ Created on Mon Nov 16 11:24:19 2020
 """
 
 import ecm
+import openpnm as op
+import matplotlib.pyplot as plt
+
+import configparser
+import os
+
+
+def _ecm_general(config_location):
+    wrk = op.Workspace()
+    wrk.clear()
+    config = configparser.ConfigParser()
+    config.read(os.path.join(config_location, 'config.txt'))
+    config.set('OUTPUT', 'save', "False")
+    config.set('OUTPUT', 'plot', "False")
+    config.set('OUTPUT', 'animate', "False")
+    I_apps = [config.get('RUN', key) for key in config['RUN'] if 'i_app' in key]
+    for I_app in I_apps:
+        save_path = config_location + '\\' + I_app + 'A'
+        prj, vrs, sols = ecm.run_simulation(float(I_app), save_path, config)
+    plt.close('all')
+    assert 1 == 1
+
+
+def test_ecm_spiral():
+    config_location = os.path.join(ecm.OUTPUT_DIR, 'spiral')
+    _ecm_general(config_location)
+
+
+def test_ecm_tomo():
+    config_location = os.path.join(ecm.OUTPUT_DIR, 'tomography')
+    _ecm_general(config_location)
+
+
+def test_ecm_1d():
+    config_location = os.path.join(ecm.OUTPUT_DIR, '1d')
+    _ecm_general(config_location)
 
 
 def test_func():
     test_config = ecm.load_test_config()
     ecm.print_config(test_config)
-
-
-if __name__ == '__main__':
-    test_func()
